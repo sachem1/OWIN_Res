5214ee5b1554926c180ba3688e3c9e1a2eeccd95
Jiesen <Jiesen@DESKTOP-3FFGJGL>
2017-08-15 15:26:27 +0800

    demo测试

---
+++ Owin_Console/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Owin.Hosting;
+
+namespace Owin_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "http://localhost:8088";
+            var startOpts=new StartOptions(url)
+            {
+                
+            };
+            using (WebApp.Start<Startup>(startOpts))
+            {
+                Console.WriteLine("Server run at "+url+",");
+            }
+        }
+    }
+}
---
+++ Owin_Console/Startup.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Owin_Console.Startup))]
+
+namespace Owin_Console
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+            app.Run(HandleRequest);
+        }
+        static Task HandleRequest(IOwinContext context)
+        {
+            context.Response.ContentType = "text/plain";
+            return context.Response.WriteAsync("Hello, world!");
+        }
+    }
+}
